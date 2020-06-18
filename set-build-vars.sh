f529d349a07e63c8a85833b58f522d617d0be4d8
Eugene Marcotte <eugene.marcotte@takeoff.com>
2020-06-18 16:28:42 -0400

    Debug logs in build var script

--- set-build-vars.sh
+++ set-build-vars.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo '##[warning] Running setup tool'
 
 # This script exists to expose some variables to azure pipelines from the repo
 # https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch
 # Figure out which branch we're on
 branch=`git rev-parse --abbrev-ref HEAD`
+echo "##[debug] $branch"
 
 if [[ "$branch" == "release" ]]; then
 	# If we're on release branch, we'll tag the image with the package version.
@@ -15,5 +15,6 @@ else
 	version=`git rev-parse HEAD`
 fi
 
+echo "##[debug] Found version $version"
 echo "##vso[task.setvariable variable=image_version]"$version
 
